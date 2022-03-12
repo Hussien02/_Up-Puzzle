1383b8c8780c0df6fb91d464b8f97c601ad958d7
bahaasrour <dev.bahaasrour@gmail.com>
2022-03-12 23:49:20 +0200

    Finish GamePlay

--- Assets/addSairsCollider.cs
+++ Assets/addSairsCollider.cs
@@ -13,4 +13,23 @@ public class addSairsCollider : MonoBehaviour
         boxCollider.enabled = true;
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement2>())
+        {
+            other.GetComponent<Animator>().enabled = true;
+            other.GetComponent<Animator>().SetBool("2", true);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement2>())
+        {
+            other.GetComponent<Animator>().SetBool("2", true);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
 }
